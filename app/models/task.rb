c488577079365eb5e5fa255fc08d7d1876f82e29
EC2 Default User <ec2-user@ip-172-31-29-254.us-west-2.compute.internal>
2020-06-29 10:09:24 +0000

    enum 追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,11 @@
 class Task < ApplicationRecord
+  belongs_to :user
+  belongs_to :desire
+
+  enum status: {
+    in_progress: 1,
+    done: 2,
+  }
+
+  scope :today, -> { where(target_date: Date.today) }
 end
