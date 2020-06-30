7ec7dcef45d7c1f3da942734637908686a9a39f8
EC2 Default User <ec2-user@ip-172-31-29-254.us-west-2.compute.internal>
2020-06-30 12:47:06 +0000

    不要なナビゲーションバー削除

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :done]
 
   # GET /tasks
   # GET /tasks.json
